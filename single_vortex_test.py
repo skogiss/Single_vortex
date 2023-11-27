d9fbc1b689b11919f6d25e7eaaf5e596f020a86a
skogiss <aisteskog@gmail.com>
2023-11-27 17:01:59 +0100

    Add support for arbitrary notch placement on right edge and arbitrary hole placement

--- single_vortex_test.py
+++ single_vortex_test.py
@@ -32,63 +32,75 @@ print(f"Applied magnetic field: {B_applied_field}{units_field}")
 #outer geometry of the SC film
 sc_film_length = 400 #1000
 sc_film_width = sc_film_length
-#sc_film= tdgl.Polygon("film", points=box(width=sc_film_length)).resample(400).buffer(0)#.resample(600)
+
+#notch geometry
+notch_ellipse_big= 50 #length of notch, 60
+notch_ellipse_small= 50 #half of height of notch, 20
+notch_length= 60
+notch_max_heigth= 40
+notch_placement_y = 0*sc_film_length
+
+def create_notch(notch_length, notch_max_heigth, notch_placement_y):
+    hole_notch=tdgl.Polygon("notch test", points=ellipse(a=notch_length,b=notch_max_heigth/2))
+    hole_notch=hole_notch.translate(dx=sc_film_width/2, dy=notch_placement_y)
+    return hole_notch
+
+notch_in_film1=create_notch(notch_length, notch_max_heigth, notch_placement_y)
+
+#create a superconducting film with a notch
+sc_film= tdgl.Polygon("film", points=box(width=sc_film_length)).difference(notch_in_film1).resample(400).buffer(0)
 
 #geometry of circular hole in the middle
+hole_radius = 2*xi_coherence
+hole_center = (-50,-20)
+
 def create_hole_round(hole_center, hole_radius):
-    #hole_radius_factor = 5 #5
-    #hole_radius = hole_radius_factor*xi_coherence
-    #hole_center = (0,0)
     hole_round = tdgl.Polygon("round hole", points=circle(radius=hole_radius, center=hole_center))
     return hole_round
 
-#notch geometry
-
-notch_ellipse_big= 60 #length of notch, 60
-notch_ellipse_small= 20 #half of height of notch, 20
-y_displacement_notch = -0.3*sc_film_length
-
-def create_notch(notch_ellipse_big, notch_ellipse_small, y_displacement_notch):
-    hole_notch=tdgl.Polygon("notch test", points=ellipse(a=notch_ellipse_big,b=notch_ellipse_small))
-    hole_notch=hole_notch.translate(dx=sc_film_width/2, dy=y_displacement_notch) #problem: dx displacement
-    #notch_diffbox=tdgl.Polygon("diffbox",points=box(width=notch_ellipse_big,height=2*notch_ellipse_small)).translate(dx=notch_ellipse_big/2)
-    #hole_notch=hole_notch.difference(notch_diffbox).buffer(0)#.translate(dx=sc_film_width/2, dy=y_displacement_notch)#.resample(401) #problem: dx displacement
-    return hole_notch
-
-hole_notch_film=create_notch(notch_ellipse_big, notch_ellipse_small, y_displacement_notch)
-
-sc_film= tdgl.Polygon("film", points=box(width=sc_film_length)).difference(hole_notch_film).resample(400).buffer(0)
-
-hole_radius = 2*xi_coherence
-hole_center = (0,0)
 hole_round= create_hole_round(hole_center, hole_radius)
 
 #track geometry
-def create_track():
-    hole_notch=hole_notch_film.difference(tdgl.Polygon(points=box(width=notch_ellipse_big,height=2*notch_ellipse_small)).translate(dx=notch_ellipse_big/2))
-    tmp_coord_notch = hole_notch.bbox
+def create_track(track_width, notch_in_film, center_of_hole):
+    #hole_notch=notch_in_film1.difference(tdgl.Polygon(points=box(width=notch_ellipse_big,height=2*notch_ellipse_small)).translate(dx=notch_ellipse_big/2))
+    tmp_coord_notch = notch_in_film.bbox
     notch_endpoint = (tmp_coord_notch[0][0], (tmp_coord_notch[0][1]+(tmp_coord_notch[1][1]-tmp_coord_notch[0][1])/2))
-    #track_length = np.linalg.norm(np.array(notch_endpoint)-np.array(hole_center))
-    track_length = np.sqrt(notch_endpoint[0]**2 + notch_endpoint[1]**2)
-    track_angle = np.arccos((hole_center[1]-notch_endpoint[1])/track_length)
-    track_angle= np.degrees(track_angle)
-    track_center=(0,0)
-    #track_center = (notch_endpoint[0]/2, notch_endpoint[1]+track_length/2)
-    track = tdgl.Polygon("track", points=box(width=0.3*xi_coherence, height=track_length, center=track_center)).rotate(degrees=track_angle,origin=track_center)#.union(hole_notch).resample(200)
-    track_notch_gap_y = notch_endpoint[1] - track.bbox[0][1]
-    track_notch_gap_x = notch_endpoint[0] - track.bbox[1][0]
+    track_length = np.sqrt((notch_endpoint[0]-center_of_hole[0])**2 + (notch_endpoint[1]-center_of_hole[1])**2)
+    adjacent_side_length = center_of_hole[1]-notch_endpoint[1]
+    opposite_side_length = center_of_hole[0]-notch_endpoint[0]
+    if adjacent_side_length==0 or opposite_side_length==0:
+        track_angle=90
+        track = tdgl.Polygon("track", points=box(width=track_width, height=track_length, center=center_of_hole, angle=track_angle))
+        track_notch_gap_x = notch_endpoint[0] - track.bbox[1][0]
+        track_notch_gap_y = 0
+    else:
+        tan_angle=opposite_side_length/adjacent_side_length
+        #track_center=center_of_hole
+        if tan_angle<0:
+            track_angle = np.arccos(adjacent_side_length/track_length)
+            track_angle= np.degrees(track_angle)
+            track = tdgl.Polygon("track", points=box(width=track_width, height=track_length, center=center_of_hole, angle=track_angle))#.rotate(degrees=track_angle,origin=track_center)
+            track_notch_gap_y = notch_endpoint[1] - track.bbox[0][1]
+            track_notch_gap_x = notch_endpoint[0] - track.bbox[1][0]
+        elif tan_angle>0:
+            track_angle = np.arccos(abs(adjacent_side_length/track_length))
+            track_angle= 180 - np.degrees(track_angle)
+            track = tdgl.Polygon("track", points=box(width=track_width, height=track_length, center=center_of_hole, angle=track_angle))#.rotate(degrees=track_angle,origin=track_center)
+            track_notch_gap_y = notch_endpoint[1] - track.bbox[1][1] #same bbox coordinates rotate too
+            track_notch_gap_x = notch_endpoint[0] - track.bbox[1][0]
+
     track=track.translate(dx=track_notch_gap_x,dy=track_notch_gap_y).difference(hole_round).resample(800)
-    #track = box(width=xi_coherence, length=track_length).rotate(track_angle)
     print(f"track box: {track.bbox}")
     print(f"notch endpoint: {notch_endpoint}, track length: {track_length}, track angle: {track_angle}")
     return track
 
+track_width = 0.3*xi_coherence
+track=create_track(track_width, notch_in_film1, hole_center)
 
-track=create_track()
-#disorder_epsilon in track
+#track dependent disorder_epsilon
 def track_epsilon(r):
     if track.contains_points(r)==True or track.on_boundary(r)==True:
-        epsilon=0.8
+        epsilon=0.5
     else:
         epsilon=1.0
     return epsilon
@@ -123,7 +135,7 @@ print(f"Vortices that can enter: {fluxoid_theoretic}")
 
 
 #solve TDGL
-tdgl_options = tdgl.SolverOptions(skip_time=0, solve_time=200,monitor=True, monitor_update_interval=0.5, field_units=units_field, current_units=units_current)
+tdgl_options = tdgl.SolverOptions(skip_time=0, solve_time=20,monitor=True, monitor_update_interval=0.5, field_units=units_field, current_units=units_current)
 solution_zero_current = tdgl.solve(sc_device, tdgl_options, applied_vector_potential=B_applied_field, disorder_epsilon=track_epsilon)
 
 fig, axes= solution_zero_current.plot_order_parameter(squared=False)
